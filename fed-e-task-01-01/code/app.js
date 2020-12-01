03697425d3694e1f124ac80bc3e8be5d95f7da5e
wangbo <wangbo@xbcx.com.cn>
2020-12-02 01:46:00 +0800

    01-01-promise手写

--- fed-e-task-01-01/code/app.js
+++ fed-e-task-01-01/code/app.js
@@ -143,5 +143,106 @@ console.log(ex4('66.6'),ex4(null),ex4(undefined),'三-练习4')
 // ### 四、手写实现 MyPromise 源码
 
 // 要求：尽可能还原 Promise 中的每一个 API，并通过注释的方式描述思路和原理。【参考代码】
-
-
+// 1.定义promise的三种状态PENDING,FULLFILLED,REJECTED
+const PENDING = 'PENDING';
+const FULLFILLED = 'FULLFILLED'
+const REJECTED = 'REJECTED'
+class MyPromise {
+    constructor(excutor){//2.首先promise接收一个立即执行函数作为参数
+        try{//20.执行器异常处理
+            excutor(this.resolve,this.reject) //3.该函数接受两个参数用于改变状态
+        }catch(e){
+            this.reject(e)
+        }
+        
+    }
+    status = PENDING;//4.定义初始状态
+    value = undefined; //7.定义值的存储，以便传递向then
+    reson = undefined; 
+    successCallback = []; //11.存储回调
+    failCallback = [] 
+    resolve = value => {//5.改变状态
+        if(this.status!==PENDING)return //6.状态一旦更改便无法再次改变
+        this.value = value  //8.获取状态改变时传入的参数存储以便于向后传递
+        this.status = FULLFILLED 
+        // this.successCallback&&this.successCallback(this.value) //12.等到状态改变时再执行回调
+        while(this.successCallback.length){  //14.依次执行多次调用then方法的函数
+            // this.successCallback.unshift()(this.value)  
+            this.successCallback.unshift()()  //23.存值是已经传递了参数，并且我们调用的是外层函数，因此不用再传值
+        }
+    }
+    reject = reason => {
+        if(this.status!==PENDING)return
+        this.reason = reason
+        this.status = REJECTED
+        // this.failCallback&&this.failCallback(this.value)
+        while(this.failCallback.length){
+            this.failCallback.unshift()()
+        }
+    }
+    then(successCallback,failCallback){//9.实现then方法，接受两个回调参数成功和失败
+        let promise2 = new Promise((resolve,reject)=>{
+            // 24.处理then方法中传递参数为空或不传时，不做处理把value或reason向下传递
+            successCallback = successCallback?successCallback:value => value
+            failCallback = failCallback?failCallback:reason => {throw reason}
+            if(this.status===FULLFILLED){
+                setTimeout(()=>{
+                   try{ //21.then方法异常处理
+                        // successCallback(this.value) 
+                        let x = successCallback(this.value) //16.存储回调参数的返回值以便向后传递.注：如何向后传递，考虑用then返回的promise对象去把返回值向下传递。考虑把整块代码移入promise对象中，既能同步执行代码，又可以拿到resolve方法以便于传递返回值。
+                        // resolve(x) 
+                        //resolvePromise(x,resolve,reject)  //17.由于x值有可能为普通值  也有可能为promise对象，因此做判断后再传递
+                        resolvePromise(promise2,x,resolve,reject)  //18.promise不允许then方法返回自身实例，因此该传入promise2做判断
+                                                                //19.此处无法获取promise2实例，巧妙利用setTimeout解决问题
+                   }catch(e){
+                       reject(e)
+                   }
+                })
+            }else if(this.status===REJECTED){
+                setTimeout(()=>{
+                    try{
+                        let x = failCallback(this.reason)
+                        resolvePromise(promise2,x,resolve,reject)
+                    }catch(e){
+                        reject(e)
+                    }
+                })
+            }else{//10.处理异步情况,调用then方法时装态还未更改，则存储回调函数，去resolve中状态更改时触发
+                // this.successCallback = successCallback  
+                // this.failCallback = failCallback
+                // this.successCallback.push(successCallback)   //13.由于函数中可以多次调用then。改进
+                // this.failCallback.push(failCallback)
+                this.successCallback.push(()=>{//22.异步的也需要如上value操作，把回调函数用函数包裹再存储
+                    setTimeout(()=>{
+                        try{
+                            let x = successCallback(this.value)
+                            resolvePromise(promise2,x,resolve,reject)
+                        }catch(e){
+                            reject(e)
+                        }
+                    })
+                })   
+                this.failCallback.push(()=>{
+                    setTimeout(()=>{
+                        try{
+                            let x = failCallback(this.reason)
+                            resolvePromise(promise2,x,resolve,reject)
+                        }catch(e){
+                            reject(e)
+                        }
+                    })
+                })
+            }
+        })
+      
+        return promise2  //15.then方法会返回一个全新的promise以实现链式调用then
+    }
+}
+const resolvePromise = (promise2,x,resolve,reject)=>{
+    if(promise2 === x){return TypeError('XXX')}//仿照promise传出错误
+    if(x instanceof MyPromise){
+        x.then(resolve,reject)
+    }else{
+        resolve(x)
+    }
+}
